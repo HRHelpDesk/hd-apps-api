0691e5a8a174677cd1792a8ee1fb8cf4e7afb895
Mason Merrell <masonmerrell@MasonsMacStudio.lan>
2023-05-03 12:23:30 -0500

    Updated jp routes

--- app.js
+++ app.js
@@ -7,7 +7,15 @@ const {PORT = 3001} = process.env;
 const axios = require('axios');
 const { uuid } = require('uuidv4');
 const nodemailer = require('nodemailer');
+const { Configuration, OpenAIApi } = require("openai");
+require('dotenv').config()
 ///Middleware
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
 let transport = nodemailer.createTransport({
   host: 'smtp.gmail.com',
   port: 465,
@@ -40,6 +48,39 @@ const user = new mongoose.Schema({
 
 const Profile = mongoose.model('user', user);
 app.use(express.json());
+
+/// End Middleware
+
+
+  app.post('/create-data',(req,res)=>{
+    const {request} = req.body;
+
+    async function runPrompt () {
+      const completion = await openai.createCompletion({
+        "model": "text-davinci-003",
+        "prompt": request,
+        "temperature": 0.6,
+        "max_tokens": 1024,
+        "stream": false
+      });
+
+      const startIndex = completion.data.choices[0].text.indexOf("{");
+      const result = completion.data.choices[0].text.substring(startIndex);
+
+      res.send(result)
+      console.log(completion.data.choices[0].text)
+      }
+    try{
+      runPrompt()
+    
+
+
+    } 
+    catch{
+      res.send('error')
+    }
+    })
+
 
 app.get('/',(req,res)=>{
 res.send('Server is awake!')
@@ -229,6 +270,166 @@ app.post('/change-iq-name',(req,res)=>{
     }
   })
   })
+
+
+
+  // JOB POST GENIE
+
+app.post('/save-job-post', (req,res)=>{
+  const {email, object} = req.body;
+  console.log(email);
+  console.log(object) 
+  Profile.find({email:email}, (err,data)=>{
+    if(err){
+      res.send('error')
+    } else {
+      if(data.length === 0){
+        let newUser = new Profile({
+          id: uuid(),
+          email:email,
+          savedQuestionairres:[],
+          jobPostings:[object],
+          jobDescriptions:[],
+          employeeHandbooks:[],
+          savedPolicies:[]
+        
+  
+       
+      })
+      newUser.save(function (err) {
+        if (err) {
+          res.send('error')
+        } else{
+          console.log('saved!')
+          res.send({
+            message:'Complete!',
+            
+            
+          })
+  
+  
+          var mailOptions = {
+            from: 'support@helpdeskforhr.com',
+            to: 'support@helpdeskforhr.com',
+            subject: `${email} created an Job Post for ${object.jobPostData.jobTitle}.`,
+            html:`<div>
+           
+            ${JSON.stringify(object)}
+            <div>`
+          };
+       
+          transport.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+   
+          
+        }
+        // saved!
+        })
+   
+  
+    } else {
+      Profile.findOneAndUpdate({email:email},{$push:{jobPostings:object}}, {'new': true, 'safe': true, 'upsert': true},(err,data)=>{
+        if(err){
+          res.send('error')
+          console.log(err)
+        } else {
+    
+          res.send({
+            message:'Complete!',
+            
+          })
+  
+  
+            var mailOptions = {
+              from: 'support@helpdeskforhr.com',
+              to: 'support@helpdeskforhr.com',
+              subject: `${email} created an Job Post for ${object.jobPostData.jobTitle}.`,
+              html:`<div>
+              ${JSON.stringify(object)}
+              <div>`
+            };
+          
+          transport.sendMail(mailOptions, function(error, info){
+            if (error) {
+              console.log(error);
+            } else {
+              console.log('Email sent: ' + info.response);
+            }
+          });
+  
+        }
+      })
+    }
+  }
+    })
+    
+  })
+
+
+  // Update
+app.post('/update-jp',(req,res)=>{
+  const {email, object} = req.body;
+  console.log(object)
+  Profile.findOneAndUpdate({email:email},{$set: {"jobPostings.$[el].jobPostData": object.jobPostData}},{arrayFilters: [{ "el.id": object.id}],'new': true, 'safe': true, 'upsert': true}, (err,data)=>{
+
+    if(err){
+      res.send('error')
+    } else {
+      res.send('updated')
+    }
+  })
+  })
+
+    // Duplicate
+app.post('/add-jp',(req,res)=>{
+  const {email, object} = req.body;
+  
+  Profile.findOneAndUpdate({email:email},{$push: {jobPostings:object}},{'new': true, 'safe': true, 'upsert': true}, (err,data)=>{
+
+    if(err){
+      res.send('error')
+    } else {
+      res.send('updated')
+    }
+  })
+  })
+
+  //Change Name
+
+  app.post('/change-jp-name',(req,res)=>{
+    const {email, id,name} = req.body;
+    
+    Profile.findOneAndUpdate({email:email},{$set: {"jobPostings.$[el].jobPostData.jobTitle": name}},{arrayFilters: [{ "el.id": id}],'new': true, 'safe': true, 'upsert': true}, (err,data)=>{
+  
+      if(err){
+        res.send('error')
+      } else {
+        res.send('updated')
+      }
+    })
+    })
+
+      // Delete
+app.post('/delete-jp',(req,res)=>{
+  const {email, iqId} = req.body;
+  
+  Profile.findOneAndUpdate({email:email},{$pull: {jobPostings:{id: iqId}}},{'new': true, 'safe': true, 'upsert': true}, (err,data)=>{
+
+    if(err){
+      res.send('error')
+    } else {
+      res.send('updated')
+    }
+  })
+  })
+  
+
+  // END Job Post Genie
 
 
 
